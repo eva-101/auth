d268b155c526fb3abaa1154038a32e42d12a1578
eva <fedeeico20052005@gmail.com>
2025-12-17 13:01:51 -0300

    Refactor server.py for improved access token handling

--- server.py
+++ server.py
@@ -234,17 +234,3 @@ if __name__ == "__main__":
     test_root()  # <-- ver qué carpetas ve Dropbox
     port = int(os.environ.get("PORT", 5000)) 
     app.run(host="0.0.0.0", port=port)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
